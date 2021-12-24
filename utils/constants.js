4872cf09f2a62208d8d3c27c2b1c124a93242993
unsteelix <unsteelix@yandex.ru>
2021-12-24 19:47:20 +0400

    fix to work

--- utils/constants.js
+++ utils/constants.js
@@ -1,10 +1,9 @@
 export const LOWBACK_URL = 'https://kali.unsteelix.keenetic.link';
 
-export const GALERA_TOKEN = process.env.NEXT_PUBLIC_GALERA_TOKEN;
+export const GALERA_TOKEN = process.env.GALERA_TOKEN;
 
-export const GALERA_PASSWORD = process.env.NEXT_PUBLIC_GALERA_PASSWORD;
+export const GALERA_PASSWORD = process.env.GALERA_PASSWORD;
 
-console.log(process.env.NEXT_PUBLIC_GALERA_TOKEN, process.env.NEXT_PUBLIC_GALERA_PASSWORD)
 
 export default {
     LOWBACK_URL,
